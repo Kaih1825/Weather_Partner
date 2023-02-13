7e6a9eb0616ce409a4b8cfa8e934bba4cbff9bd1
Kai <imatch11@gmail.com>
2023-02-13 17:34:40 +0800

    Add center column to login screen.

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -13,50 +13,56 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent
-      ),
+      value: const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
       child: Scaffold(
         body: Container(
+          width: double.infinity,
+          height: double.infinity,
           decoration: const BoxDecoration(
               gradient: LinearGradient(colors: [
             Color.fromARGB(255, 0, 147, 233),
             Color.fromARGB(255, 128, 208, 199)
           ], begin: Alignment.topRight, end: Alignment.bottomLeft)),
-          child: Column(
-            children: [
-              Expanded(
-                child: Stack(
-                  children: const [
-                    Positioned(
-                    width: 200,
-                    height: 200,
-                    top: -40,
-                    right: -50,
-                    child: Circle(),
+          child: Center(
+            child: Stack(children: [
+              Column(
+                children: [
+                  Expanded(
+                    child: Stack(children: const [
+                      Positioned(
+                        width: 200,
+                        height: 200,
+                        top: -40,
+                        right: -50,
+                        child: Circle(),
+                      ),
+                    ]),
                   ),
-                  ]
-                ),
+                  Expanded(
+                    child: Stack(children: const [
+                      Positioned(
+                        width: 200,
+                        height: 200,
+                        bottom: -80,
+                        left: -40,
+                        child: Circle(),
+                      ),
+                    ]),
+                  ),
+                ],
               ),
-              Expanded(
-                child: Stack(
-                  children: const [
-                    Positioned(
-                    width: 200,
-                    height: 200,
-                    bottom: -80,
-                    left: -40,
-                    child: Circle(),
-                  ),
-                  ]
+              SafeArea(
+                  child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [Text('data')],
                 ),
-              )
-            ],
+              ))
+            ]),
           ),
         ),
       ),
     );
   }
 }
-
-
