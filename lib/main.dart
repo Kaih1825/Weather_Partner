c9ab814f57728747d70cbe95197459f32b1e51aa
Kai <imatch11@gmail.com>
2023-09-06 23:08:14 +0800

    Add Dynamic Color support use Material 3 design.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,48 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
-import 'package:weather_partner/Screens/LoginScreen.dart';
 
 void main() {
   runApp(const Main());
 }
 
 class Main extends StatefulWidget {
-  const Main({super.key});
+  const Main({Key? key}) : super(key: key);
 
   @override
   State<Main> createState() => _MainState();
 }
 
 class _MainState extends State<Main> {
+  static final _defaultLightColorScheme = ColorScheme.fromSwatch(primarySwatch: Colors.pink, brightness: Brightness.light);
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(primarySwatch: Colors.blue, brightness: Brightness.dark);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const LoginScreen(),
+    return DynamicColorBuilder(
+      builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(useMaterial3: true, colorScheme: lightDynamic ?? _defaultLightColorScheme),
+          darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark, colorScheme: darkDynamic ?? _defaultDarkColorScheme),
+          home: Scaffold(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            body: Center(
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Save",
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
